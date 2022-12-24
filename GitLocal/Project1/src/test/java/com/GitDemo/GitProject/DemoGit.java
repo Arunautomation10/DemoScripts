09feccdbc564922b6923cd8b7fdbd2d5b7694b23
Arunautomation10 <auto.prac.test@gmail.com>
2022-12-24 19:59:48 +0530

    commit new changes

--- GitLocal/Project1/src/test/java/com/GitDemo/GitProject/DemoGit.java
+++ GitLocal/Project1/src/test/java/com/GitDemo/GitProject/DemoGit.java
@@ -10,6 +10,9 @@ public class DemoGit {
 		String rev = sb.reverse().toString();
 		System.out.println(rev);
 		
+		String sb1 = new StringBuffer("Shravani").reverse().toString();
+		System.out.println(sb1);
+		
 		for(int i=name.length()-1; i>=0; i--)
 		{
 			System.out.print(name.charAt(i));
